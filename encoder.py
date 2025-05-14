7ca1956be751e62ab86e7ab598d98fdf2f319f5c
tEokolE160 <logos.basque02@icloud.com>
2025-05-14 22:15:17 +0200

    解决了 空指针异常 Bug

---
+++ encoder.py
@@ -0,0 +1,13 @@
+import base64
+
+
+def encode(chunk):
+    enc = str(base64.encodebytes(chunk), 'utf-8')
+    return enc
+
+
+def decode(chunk):
+    missing_padding = len(chunk) % 4
+    if missing_padding != 0:
+        chunk += b'=' * (4 - missing_padding)
+    return base64.decodebytes(chunk)
